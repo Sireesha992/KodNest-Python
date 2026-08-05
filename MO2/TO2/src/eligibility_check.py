dfc07e8b52299e6883793acf59ebb8fa48a4b441
Sireesha992 <sireeshayalavarthi97@gmail.com>
2026-08-05 21:39:49 +0530

    eligibility_check.py

---
+++ MO2/TO2/src/eligibility_check.py
@@ -0,0 +1,11 @@
+marks = 78
+attendance = 82
+project_completion_status = "yes"
+if marks >= 60 and attendance >=75:
+    if project_completion_status == "yes":
+        print("Eligible")
+    else:
+        print("Not Eligible")
+else:
+    print("Not Eligible")
+
