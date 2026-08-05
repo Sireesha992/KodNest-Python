ea809f8736bd0bd46f77e5d2570e58971eb1c857
Sireesha992 <sireeshayalavarthi97@gmail.com>
2026-08-05 20:53:11 +0530

    read_student.py

---
+++ track/M02/TO1/src/read_student.py
@@ -0,0 +1,9 @@
+student_name = "Meera"
+student_age = 21
+course_rating =4.8
+print(f"Student: {student_name}")
+print(f"Age: {student_age}")
+print(f"Rating: {course_rating}")
+print(type(student_name))
+print(type(student_age))
+print(type(course_rating))
