e7c4a9ebd4457de443c121f27a3d56dfba7aef94
Sireesha992 <sireeshayalavarthi97@gmail.com>
2026-08-10 18:45:25 +0530

    add slicing.py

---
+++ TO3/src/slicing.py
@@ -0,0 +1,9 @@
+word = "Python"
+first = 10
+second = 20
+third = 30
+numbers = [first, second, third]
+record = (first, second, third)
+print("Middle:",word[1:-1])
+print("First Number:",numbers[:2])
+print("Reversed Tuple",record[::-1])
