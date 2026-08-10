fc614d7ab5929edaed4d5bb07de2b807b90d3474
Sireesha992 <sireeshayalavarthi97@gmail.com>
2026-08-10 18:37:55 +0530

    update_tuple.py

---
+++ TO3/src/update_tuple.py
@@ -0,0 +1,8 @@
+course_name = "Python"
+current_week = "week 1"
+current_status = "Active"
+updated_week = "week 2"
+course_details = (course_name, current_week, current_status)
+course_details = (course_details[0],updated_week,course_details[2])
+print(course_details)
+
