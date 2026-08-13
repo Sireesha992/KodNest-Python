3780c506e858e9fa1ebc51c130c724e776ad114d
Sireesha992 <sireeshayalavarthi97@gmail.com>
2026-08-13 21:41:20 +0530

    shared.py

---
+++ TO4/src/shared.py
@@ -0,0 +1,9 @@
+def add_student(name, students=[]):
+    students.append(name)
+    print(students)
+first_name = "Asha"
+second_name = "Ravi"
+third_name = "Meera"
+add_student(first_name)
+add_student(second_name)
+add_student(third_name)
